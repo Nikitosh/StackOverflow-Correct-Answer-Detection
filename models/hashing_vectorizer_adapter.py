7bab58e9d709cd9f80223219e9d2636498649a24
Nikitosh <npodguzov@yandex.ru>
2018-04-24 00:17:56 +0300

    Add two vectorizer adapters for using in sklearn classifiers

---
+++ models/hashing_vectorizer_adapter.py
@@ -0,0 +1,12 @@
+from sklearn.feature_extraction.text import HashingVectorizer
+
+
+class HashingVectorizerAdapter:
+    def __init__(self, **kwargs):
+        self.vectorizer = HashingVectorizer(**kwargs)
+
+    def fit(self, data_reader, ids):
+        pass
+
+    def transform(self, X):
+        return self.vectorizer.transform(X)
