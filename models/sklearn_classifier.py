7f486f18f5f916d75d5e9aa4ef649981ab5545fa
Nikitosh <npodguzov@yandex.ru>
2018-04-24 00:21:21 +0300

    Update sklearn classifier to use vectorizer adapters

--- models/sklearn_classifier.py
+++ models/sklearn_classifier.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-from model.data_reader import DataReader
+from models.data_reader import DataReader
+from models.utils import print_metrics, transform_text, stem_text
 
 
 class SKLearnClassifier:
@@ -17,19 +17,16 @@ class SKLearnClassifier:
         vectorizer.fit(data_reader, train_ids)
 
         all_classes = np.array([0, 1])
-        for X_train, y_train in data_reader.get_texts_labels_batch(set(train_ids), self.batch_size):
-            X_train = vectorizer.transform(X_train)
+        for X_train, y_train in data_reader.get_data_labels_batch(set(train_ids), self.batch_size):
+            X_train = vectorizer.transform(stem_text(X) for X in X_train['body'])
             classifier.partial_fit(X_train, y_train, classes=all_classes)
 
         y_tests = []
         y_preds = []
-        for X_test, y_test in data_reader.get_texts_labels_batch(set(test_ids), self.batch_size):
-            X_test = vectorizer.transform(X_test)
+        for X_test, y_test in data_reader.get_data_labels_batch(set(test_ids), self.batch_size):
+            X_test = vectorizer.transform(stem_text(X) for X in X_test['body'])
             y_pred = classifier.predict(X_test)
             y_tests.extend(y_test)
             y_preds.extend(y_pred)
 
-        print('Accuracy: {}'.format(accuracy_score(y_tests, y_preds)))
-        print('Precision: {}'.format(precision_score(y_tests, y_preds)))
-        print('Recall: {}'.format(recall_score(y_tests, y_preds)))
-        print('F1: {}'.format(f1_score(y_tests, y_preds)))
+        print_metrics(y_tests, y_preds)
