e23f57df5cd5fc1310fae32c228eb831efa40fc0
Nikitosh <npodguzov@yandex.ru>
2018-04-26 22:36:24 +0300

    Update model to train on wiki corpus

--- word2vec/word2vec_model_trainer.py
+++ word2vec/word2vec_model_trainer.py
@@ -1,18 +1,19 @@
-from gensim.models import KeyedVectors
-from gensim.models import Word2Vec
-from sklearn.cross_validation import train_test_split
+import gensim.downloader as api
 
-from models.data_reader import DataReader
+from gensim.models import Word2Vec
 
 
 class Word2VecModelTrainer:
+    def train_on_wiki(self):
+        data = api.load('wiki-english-20171001')
+        print(len(data))
+        word2vec_model = Word2Vec(data)
+        word2vec_model.save('word2vec/models/model.bin')
+
     def train(self, csv_file_name):
-        data_reader = DataReader(csv_file_name)
-        word2vec_model = Word2Vec()
-        word2vec_vectors = KeyedVectors.load_word2vec_format('word2vec/models/google_news_vectors.bin', binary=True)
-        word2vec_model.wv = word2vec_vectors
-        ids = data_reader.get_ids()
-        train_ids, test_ids = train_test_split(ids, random_state=0)
-        word2vec_model.build_vocab(data_reader.get_texts(train_ids), update=True)
-        word2vec_model.train(data_reader.get_texts(train_ids))
-        word2vec_model.save('word2vec/models/google_news_updated.bin')
+        pass
+        #data_reader = DataReader(csv_file_name)
+        #ids = data_reader.get_ids()
+        #train_ids, test_ids = train_test_split(ids, random_state=0)
+        #word2vec_model.build_vocab(data_reader.get_texts(train_ids), update=True)
+        #word2vec_model.train(data_reader.get_texts(train_ids))
