ba0661b5753903927cbad93391fcd5dd636b7635
Nikitosh <npodguzov@yandex.ru>
2018-04-26 18:33:12 +0300

    Add script for downloading data

---
+++ scripts/download.sh
@@ -0,0 +1,6 @@
+wget -P SO_data "https://archive.org/download/stackexchange/$1.7z"
+7z e -oSO_data "SO_data/$1.7z"
+rm -rf "SO_data/$1.7z" SO_data/Badges.xml SO_data/Comments.xml SO_data/PostHistory.xml SO_data/PostLinks.xml SO_data/Tags.xml SO_data/Users.xml SO_data/Votes.xml
+mv SO_data/Posts.xml "SO_data/$2.xml"
+python3 preprocessors/xml_to_csv_preprocessor.py --xml_path "SO_data/$2.xml"
+rm "SO_data/$2.xml"
