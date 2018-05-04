36a316a846faf1f17ac48df8ea84c7e05bb3d09f
Nikitosh <npodguzov@yandex.ru>
2018-05-04 20:53:18 +0300

    Add model pretraining on wiki articles and training on SO data

---
+++ word2vec/wiki_sentences.py
@@ -0,0 +1,20 @@
+import gensim.downloader as api
+
+from models.utils import lower_text
+
+
+class WikiSentences:
+    MIN_SENTENCE_LENGTH = 10
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __iter__(self):
+        data = api.load(self.filename)
+        for article in data:
+            for text in article['section_texts']:
+                sentences = text.split('.')
+                for sentence in sentences:
+                    sentence = lower_text(sentence)
+                    if len(sentence) >= WikiSentences.MIN_SENTENCE_LENGTH:
+                        yield sentence.split()
--- word2vec/word2vec_model_trainer.py
+++ word2vec/word2vec_model_trainer.py
@@ -1,19 +1,30 @@
-import gensim.downloader as api
+import os.path
 
 from gensim.models import Word2Vec
+from sklearn.model_selection import train_test_split
+
+from models.data_reader import DataReader
+from word2vec.wiki_sentences import WikiSentences
 
 
 class Word2VecModelTrainer:
+    WIKI_MODEL_PATH = 'word2vec/models/wiki_model.bin'
+    SO_MODEL_PATH = 'word2vec/models/so_model.bin'
+
     def train_on_wiki(self):
-        data = api.load('wiki-english-20171001')
-        print(len(data))
-        word2vec_model = Word2Vec(data)
-        word2vec_model.save('word2vec/models/model.bin')
+        sentences = WikiSentences('wiki-english-20171001')
+        word2vec_model = Word2Vec(sentences, size=300)
+        word2vec_model.save(Word2VecModelTrainer.WIKI_MODEL_PATH)
 
     def train(self, csv_file_name):
-        pass
-        #data_reader = DataReader(csv_file_name)
-        #ids = data_reader.get_ids()
-        #train_ids, test_ids = train_test_split(ids, random_state=0)
-        #word2vec_model.build_vocab(data_reader.get_texts(train_ids), update=True)
-        #word2vec_model.train(data_reader.get_texts(train_ids))
+        if os.path.isfile(Word2VecModelTrainer.SO_MODEL_PATH):
+            return
+        if not os.path.isfile(Word2VecModelTrainer.WIKI_MODEL_PATH):
+            self.train_on_wiki()
+        word2vec_model = Word2Vec.load(Word2VecModelTrainer.WIKI_MODEL_PATH)
+        data_reader = DataReader(csv_file_name)
+        ids = data_reader.get_ids()
+        train_ids, test_ids = train_test_split(ids, random_state=0)
+        word2vec_model.build_vocab(data_reader.get_texts(train_ids), update=True)
+        word2vec_model.train(data_reader.get_texts(train_ids))
+        word2vec_model.save(Word2VecModelTrainer.SO_MODEL_PATH)
