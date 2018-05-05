fffd3058f3e6293be83a2adde01587a80c983e8f
Nikitosh <npodguzov@yandex.ru>
2018-05-05 23:37:02 +0300

    Update Word2Vec model trainer

--- word2vec/word2vec_model_trainer.py
+++ word2vec/word2vec_model_trainer.py
@@ -4,12 +4,11 @@ from gensim.models import Word2Vec
 from sklearn.model_selection import train_test_split
 
 from models.data_reader import DataReader
+from models.utils import get_word2vec_model_path
 from word2vec.wiki_sentences import WikiSentences
-
 
 class Word2VecModelTrainer:
     WIKI_MODEL_PATH = 'word2vec/models/wiki_model.bin'
-    SO_MODEL_PATH = 'word2vec/models/so_model.bin'
 
     def train_on_wiki(self):
         sentences = WikiSentences('wiki-english-20171001')
@@ -17,7 +16,8 @@ class Word2VecModelTrainer:
         word2vec_model.save(Word2VecModelTrainer.WIKI_MODEL_PATH)
 
     def train(self, csv_file_name):
-        if os.path.isfile(Word2VecModelTrainer.SO_MODEL_PATH):
+        model_path = get_word2vec_model_path(csv_file_name)
+        if os.path.isfile(model_path):
             return
         if not os.path.isfile(Word2VecModelTrainer.WIKI_MODEL_PATH):
             self.train_on_wiki()
@@ -26,5 +26,6 @@ class Word2VecModelTrainer:
         ids = data_reader.get_ids()
         train_ids, test_ids = train_test_split(ids, random_state=0)
         word2vec_model.build_vocab(data_reader.get_texts(train_ids), update=True)
-        word2vec_model.train(data_reader.get_texts(train_ids))
-        word2vec_model.save(Word2VecModelTrainer.SO_MODEL_PATH)
+        word2vec_model.train(data_reader.get_texts(train_ids), epochs=word2vec_model.iter,
+                             total_examples=word2vec_model.corpus_count)
+        word2vec_model.save(model_path)
