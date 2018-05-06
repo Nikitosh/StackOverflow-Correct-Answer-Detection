07b9927673d0b4fb59973ceb7a7a9247d20a1991
Nikitosh <npodguzov@yandex.ru>
2018-05-06 17:50:10 +0300

    Update Word2Vec trainer with new interface

--- word2vec/word2vec_model_trainer.py
+++ word2vec/word2vec_model_trainer.py
@@ -1,9 +1,7 @@
 import os.path
 
 from gensim.models import Word2Vec
-from sklearn.model_selection import train_test_split
 
-from models.data_reader import DataReader
 from models.utils import get_word2vec_model_path
 from word2vec.wiki_sentences import WikiSentences
 
@@ -15,18 +13,15 @@ class Word2VecModelTrainer:
         word2vec_model = Word2Vec(sentences, size=300)
         word2vec_model.save(Word2VecModelTrainer.WIKI_MODEL_PATH)
 
-    def train(self, csv_file_name):
-        model_path = get_word2vec_model_path(csv_file_name)
+    def train(self, data_reader, ids):
+        model_path = get_word2vec_model_path(data_reader.csv_file_name)
         if os.path.isfile(model_path):
             return
         if not os.path.isfile(Word2VecModelTrainer.WIKI_MODEL_PATH):
             self.train_on_wiki()
         word2vec_model = Word2Vec.load(Word2VecModelTrainer.WIKI_MODEL_PATH)
-        data_reader = DataReader(csv_file_name)
-        ids = data_reader.get_ids()
-        train_ids, test_ids = train_test_split(ids, random_state=0)
-        word2vec_model.min_count = 3
-        word2vec_model.build_vocab(data_reader.get_texts_as_lists(train_ids), update=True)
-        word2vec_model.train(data_reader.get_texts_as_lists(train_ids), epochs=word2vec_model.iter,
+        word2vec_model.min_count = 1
+        word2vec_model.build_vocab(data_reader.get_texts_as_lists(ids), update=True)
+        word2vec_model.train(data_reader.get_texts_as_lists(ids), epochs=word2vec_model.iter,
                              total_examples=word2vec_model.corpus_count)
         word2vec_model.save(model_path)
