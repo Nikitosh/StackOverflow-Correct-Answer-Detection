b524d4e31bc2eca58d980598cb0f7538ef2cb560
Nikitosh <npodguzov@yandex.ru>
2018-05-07 16:51:22 +0300

    Add epochs in train

--- train.py
+++ train.py
@@ -5,14 +5,15 @@ from models.data_reader import DataReader
 from models.utils import print_metrics
 
 
-def train(classifier, csv_file_name, batch_size=50):
+def train(classifier, csv_file_name, batch_size=50, epochs=1):
     data_reader = DataReader(csv_file_name)
     ids = data_reader.get_ids()
-    train_ids, test_ids = train_test_split(ids, random_state=1)
+    train_ids, test_ids = train_test_split(ids, random_state=3)
 
     classifier.pretrain(data_reader, train_ids)
-    for X_train, y_train in data_reader.get_data_labels_batch(set(train_ids), batch_size):
-        classifier.train_on_batch(X_train, y_train)
+    for epoch in range(epochs):
+        for X_train, y_train in data_reader.get_data_labels_batch(set(train_ids), batch_size):
+            classifier.train_on_batch(X_train, y_train)
 
     y_tests = []
     y_preds = []
