5b65cc03086dbcff0459cd796463c86231a51e80
Nikitosh <npodguzov@yandex.ru>
2018-05-10 02:15:41 +0300

    Add classifiers with returned loss

--- models/sklearn_classifier.py
+++ models/sklearn_classifier.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from models.utils import stem_text
+from utils.utils import stem_text
 
 
 class SKLearnClassifier:
@@ -15,6 +15,7 @@ class SKLearnClassifier:
         all_classes = np.array([0, 1])
         X = self.vectorizer.transform(stem_text(text) for text in X['body'])
         self.classifier.partial_fit(X, y, classes=all_classes)
+        return [0, 0]
 
     def predict(self, X):
         X = self.vectorizer.transform(stem_text(text) for text in X['body'])
