32346d7fc74342cb601f8b2d35d2610eccd40359
Nikitosh <npodguzov@yandex.ru>
2018-05-10 02:15:01 +0300

    Update files using data_reader

--- preprocessors/csv_to_fasttext_preprocessor.py
+++ preprocessors/csv_to_fasttext_preprocessor.py
@@ -1,16 +1,16 @@
-from models.utils import lower_text
+from utils.utils import lower_text, process_html
 
 
 class CsvToFasttextPreprocessor:
 
     def process_train(self, fasttext_file_name, data_reader, ids, batch_size=50):
         with open(fasttext_file_name, 'w', encoding='utf-8') as fasttext_file:
-            for X, y in data_reader.get_data_labels_batch(set(ids), batch_size):
+            for X, y in data_reader.get_raw_data_labels_batch(set(ids), batch_size):
                 for i in range(len(y)):
-                    fasttext_file.write('__label__{} {}\n'.format(y[i], lower_text(X['body'].iloc[i])))
+                    fasttext_file.write('__label__{} {}\n'.format(y[i], lower_text(process_html(X['body'].iloc[i]))))
 
     def process_test(self, fasttext_file_name, data_reader, ids, batch_size=50):
         with open(fasttext_file_name, 'w', encoding='utf-8') as fasttext_file:
-            for X, y in data_reader.get_data_labels_batch(set(ids), batch_size):
+            for X, y in data_reader.get_raw_data_labels_batch(set(ids), batch_size):
                 for i in range(len(y)):
-                    fasttext_file.write(lower_text(X['body'].iloc[i]) + '\n')
+                    fasttext_file.write(lower_text(process_html(X['body'].iloc[i])) + '\n')
