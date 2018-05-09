f7de8d13dceab6de48f4d6aaf242a5544831f98a
Nikitosh <npodguzov@yandex.ru>
2018-05-10 02:17:40 +0300

    Add logging of losses, accuracies, auc and ap in train

--- train.py
+++ train.py
@@ -1,8 +1,10 @@
+import logging
+
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from models.data_reader import DataReader
-from models.utils import print_metrics
+from data_reader.data_reader import DataReader
+from utils.utils import print_metrics
 
 
 def train(classifier, csv_file_name, batch_size=50, epochs=1):
@@ -12,13 +14,21 @@ def train(classifier, csv_file_name, batch_size=50, epochs=1):
 
     classifier.pretrain(data_reader, train_ids)
     for epoch in range(epochs):
-        for X_train, y_train in data_reader.get_data_labels_batch(set(train_ids), batch_size):
-            classifier.train_on_batch(X_train, y_train)
+        logging.info('Epoch {}/{}'.format(epoch + 1, epochs))
+        losses = []
+        accuracies = []
+        for X_train, y_train in data_reader.get_raw_data_labels_batch(set(train_ids), batch_size):
+            result = classifier.train_on_batch(X_train, y_train)
+            losses.append(result[0])
+            accuracies.append(result[1])
+            logging.info('Loss: {}, accuracy: {}'.format(result[0], result[1]))
+        logging.info('Mean loss for epoch {}: {}'.format(epoch + 1, np.mean(losses)))
+        logging.info('Mean accuracy for epoch {}: {}'.format(epoch + 1, np.mean(accuracies)))
 
     y_tests = []
     y_preds = []
-    for X_test, y_test in data_reader.get_data_labels_batch(set(test_ids), batch_size):
-        y_pred = np.round(classifier.predict(X_test))
+    for X_test, y_test in data_reader.get_raw_data_labels_batch(set(test_ids), batch_size):
+        y_pred = classifier.predict(X_test)
         y_tests.extend(y_test)
         y_preds.extend(y_pred)
 
