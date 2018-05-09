4732fdf390d62c4befc3002b1a58f0f1558938f3
Nikitosh <npodguzov@yandex.ru>
2018-05-10 02:15:41 +0300

    Add classifiers with returned loss

--- models/rnn_word2vec_classifier.py
+++ models/rnn_word2vec_classifier.py
@@ -1,27 +1,29 @@
-import numpy as np
+import logging
+
 from keras import Sequential
 from keras.layers import Dropout, Dense
 
 from gensim.models.word2vec import Word2Vec
-from models.utils import transform_sentence_batch_to_vector, lower_text, get_word2vec_model_path, add_lstm_to_model
+from utils.utils import transform_sentence_batch_to_vector, lower_text, get_word2vec_model_path, add_lstm_to_model, \
+    process_html
 from word2vec.word2vec_model_trainer import Word2VecModelTrainer
 
 
 class RnnWord2VecClassifier:
-    def __init__(self, bidirectional=False, dropout=0.1):
+    def __init__(self, answer_body_words_count, lstm_embed_size, bidirectional, dropout):
+        self.answer_body_words_count = answer_body_words_count
         self.num_features = 300
-        self.document_max_num_words = 300
-        self.embed_size = 100
 
         self.word_vectors = None
 
         self.model = Sequential()
-        input_shape = (self.document_max_num_words, self.num_features)
-        add_lstm_to_model(self.model, self.embed_size, bidirectional, input_shape, dropout)
-        self.model.add(Dropout(0.2))
+        input_shape = (self.answer_body_words_count, self.num_features)
+        add_lstm_to_model(self.model, input_shape, lstm_embed_size, bidirectional, dropout)
+        self.model.add(Dropout(dropout))
         self.model.add(Dense(1, activation='sigmoid'))
         self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-        print(self.model.summary())
+
+        self.model.summary(print_fn=logging.info)
 
     def pretrain(self, data_reader, ids):
         trainer = Word2VecModelTrainer()
@@ -29,11 +31,13 @@ class RnnWord2VecClassifier:
         self.word_vectors = Word2Vec.load(get_word2vec_model_path(data_reader.csv_file_name)).wv
 
     def train_on_batch(self, X, y):
-        X = transform_sentence_batch_to_vector(self.word_vectors, [lower_text(text) for text in X['body']],
-                                               self.document_max_num_words, self.num_features)
-        self.model.train_on_batch(X, y)
+        X = transform_sentence_batch_to_vector(self.word_vectors,
+                                               [lower_text(process_html(text)) for text in X['body']],
+                                               self.answer_body_words_count, self.num_features)
+        return self.model.train_on_batch(X, y)
 
     def predict(self, X):
-        X = transform_sentence_batch_to_vector(self.word_vectors, [lower_text(text) for text in X['body']],
-                                               self.document_max_num_words, self.num_features)
-        return np.round(self.model.predict(X))
+        X = transform_sentence_batch_to_vector(self.word_vectors,
+                                               [lower_text(process_html(text)) for text in X['body']],
+                                               self.answer_body_words_count, self.num_features)
+        return self.model.predict(X)
--- models/rnn_word2vec_with_question_classifier.py
+++ models/rnn_word2vec_with_question_classifier.py
@@ -1,58 +1,60 @@
-import numpy as np
+import logging
+
 from keras import Sequential
 from keras.layers import Dropout, Dense, Merge
 
 from gensim.models.word2vec import Word2Vec
-from models.utils import transform_sentence_batch_to_vector, lower_text, get_word2vec_model_path, add_lstm_to_model
+from utils.utils import transform_sentence_batch_to_vector, lower_text, get_word2vec_model_path, add_lstm_to_model, \
+    process_html
 from word2vec.word2vec_model_trainer import Word2VecModelTrainer
 
 
 class RnnWord2VecWithQuestionClassifier:
-    def __init__(self, bidirectional=False, dropout=0.1):
-        self.batch_size = 50
+    def __init__(self, question_title_words_count, question_body_words_count, answer_body_words_count, lstm_embed_size,
+                 bidirectional, dropout, mode):
+        self.question_title_words_count = question_title_words_count
+        self.question_body_words_count = question_body_words_count
+        self.answer_body_words_count = answer_body_words_count
         self.num_features = 300
-        self.question_title_max_num_words = 50
-        self.question_body_max_num_words = 500
-        self.answer_body_max_num_words = 500
-        self.embed_size = 100
 
         self.word_vectors = None
 
         self.question_title_model = Sequential()
-        question_title_input_shape = (self.question_title_max_num_words, self.num_features)
-        add_lstm_to_model(self.question_title_model, self.embed_size, bidirectional, question_title_input_shape, dropout)
+        question_title_input_shape = (self.question_title_words_count, self.num_features)
+        add_lstm_to_model(self.question_title_model, lstm_embed_size, bidirectional, question_title_input_shape,
+                          dropout)
         self.question_title_model.add(Dropout(dropout))
 
         self.question_body_model = Sequential()
-        question_body_input_shape = (self.question_body_max_num_words, self.num_features)
-        add_lstm_to_model(self.question_body_model, self.embed_size, bidirectional, question_body_input_shape, dropout)
+        question_body_input_shape = (self.question_body_words_count, self.num_features)
+        add_lstm_to_model(self.question_body_model, lstm_embed_size, bidirectional, question_body_input_shape, dropout)
         self.question_body_model.add(Dropout(dropout))
 
         self.answer_body_model = Sequential()
-        answer_body_input_shape = (self.answer_body_max_num_words, self.num_features)
-        add_lstm_to_model(self.answer_body_model, self.embed_size, bidirectional, answer_body_input_shape, dropout)
+        answer_body_input_shape = (self.answer_body_words_count, self.num_features)
+        add_lstm_to_model(self.answer_body_model, lstm_embed_size, bidirectional, answer_body_input_shape, dropout)
         self.answer_body_model.add(Dropout(dropout))
 
         self.model = Sequential()
-        self.model.add(Merge([self.question_title_model, self.question_body_model, self.answer_body_model], mode='concat'))
+        self.model.add(Merge([self.question_title_model, self.question_body_model, self.answer_body_model], mode=mode))
         self.model.add(Dense(1, activation='sigmoid'))
         self.model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-        print(self.question_title_model.summary())
-        print(self.question_body_model.summary())
-        print(self.answer_body_model.summary())
-        print(self.model.summary())
+        self.question_title_model.summary(print_fn=logging.info)
+        self.question_body_model.summary(print_fn=logging.info)
+        self.answer_body_model.summary(print_fn=logging.info)
+        self.model.summary(print_fn=logging.info)
 
     def get_X(self, word_vectors, data):
         question_title = transform_sentence_batch_to_vector(word_vectors,
-                                                            [lower_text(X) for X in data['question_title']],
-                                                            self.question_title_max_num_words, self.num_features)
+                                                            [lower_text(process_html(X)) for X in data['question_title']],
+                                                            self.question_title_words_count, self.num_features)
         question_body = transform_sentence_batch_to_vector(word_vectors,
-                                                           [lower_text(X) for X in data['question_body']],
-                                                           self.question_body_max_num_words, self.num_features)
+                                                           [lower_text(process_html(X)) for X in data['question_body']],
+                                                           self.question_body_words_count, self.num_features)
         answer_body = transform_sentence_batch_to_vector(word_vectors,
-                                                         [lower_text(X) for X in data['body']],
-                                                         self.answer_body_max_num_words, self.num_features)
+                                                         [lower_text(process_html(X)) for X in data['body']],
+                                                         self.answer_body_words_count, self.num_features)
         return [question_title, question_body, answer_body]
 
     def pretrain(self, data_reader, ids):
@@ -61,7 +63,7 @@ class RnnWord2VecWithQuestionClassifier:
         self.word_vectors = Word2Vec.load(get_word2vec_model_path(data_reader.csv_file_name)).wv
 
     def train_on_batch(self, X, y):
-        self.model.train_on_batch(self.get_X(self.word_vectors, X), y)
+        return self.model.train_on_batch(self.get_X(self.word_vectors, X), y)
 
     def predict(self, X):
-        return np.round(self.model.predict(self.get_X(self.word_vectors, X)))
+        return self.model.predict(self.get_X(self.word_vectors, X))
