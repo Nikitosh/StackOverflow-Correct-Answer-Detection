5ab0fc08373a86516349903f7c9f5216c18ffa19
Nikitosh <npodguzov@yandex.ru>
2018-05-10 02:15:01 +0300

    Update files using data_reader

--- models/tfidf_vectorizer_adapter.py
+++ models/tfidf_vectorizer_adapter.py
@@ -6,7 +6,7 @@ class TfIdfVectorizerAdapter:
         self.vectorizer = TfidfVectorizer(**kwargs)
 
     def fit(self, data_reader, ids):
-        self.vectorizer.fit(data_reader.get_texts(ids))
+        self.vectorizer.fit(data_reader.get_stemmed_texts(ids))
 
     def transform(self, X):
         return self.vectorizer.transform(X)
