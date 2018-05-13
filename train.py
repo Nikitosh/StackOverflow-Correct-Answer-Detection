96e48be492d5da0e70c0d813570c8f816f64ab61
Nikitosh <npodguzov@yandex.ru>
2018-05-13 19:27:50 +0300

    Add validation testing and model saving

--- train.py
+++ train.py
@@ -3,14 +3,15 @@ import logging
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from data_reader.data_reader import DataReader
+from utils.data_reader import DataReader
 from utils.utils import print_metrics
 
 
 def train(classifier, csv_file_name, batch_size=50, epochs=1):
     data_reader = DataReader(csv_file_name)
     ids = data_reader.get_ids()
-    train_ids, test_ids = train_test_split(ids, random_state=3)
+    train_ids, test_ids = train_test_split(ids, test_size=0.2, random_state=1)
+    train_ids, val_ids = train_test_split(train_ids, test_size=0.2, random_state=1)
 
     classifier.pretrain(data_reader, train_ids)
     for epoch in range(epochs):
@@ -22,8 +23,19 @@ def train(classifier, csv_file_name, batch_size=50, epochs=1):
             losses.append(result[0])
             accuracies.append(result[1])
             logging.info('Loss: {}, accuracy: {}'.format(result[0], result[1]))
-        logging.info('Mean loss for epoch {}: {}'.format(epoch + 1, np.mean(losses)))
-        logging.info('Mean accuracy for epoch {}: {}'.format(epoch + 1, np.mean(accuracies)))
+        logging.info('Mean training loss for epoch {}: {}'.format(epoch + 1, np.mean(losses)))
+        logging.info('Mean training accuracy for epoch {}: {}'.format(epoch + 1, np.mean(accuracies)))
+
+        losses = []
+        accuracies = []
+        for X_val, y_val in data_reader.get_raw_data_labels_batch(set(val_ids), batch_size):
+            result = classifier.evaluate(X_val, y_val)
+            losses.append(result[0])
+            accuracies.append(result[1])
+        logging.info('Mean validation loss for epoch {}: {}'.format(epoch + 1, np.mean(losses)))
+        logging.info('Mean validation accuracy for epoch {}: {}'.format(epoch + 1, np.mean(accuracies)))
+
+        classifier.save(epoch + 1)
 
     y_tests = []
     y_preds = []
